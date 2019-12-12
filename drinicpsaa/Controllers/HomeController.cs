7290f3804f44ec8a6e61f430d3c0c46096e18a57
Strahinja <strahinjadrinic@yahoo.com>
2019-12-13 00:11:31 +0100

    Initial commit

---
+++ drinicpsaa/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace drinicpsaa.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Omeni()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Unesite vase podatke!";
+            ViewBag.Title = "Kontaktirajte me!";
+            return View();
+        }
+    }
+}
